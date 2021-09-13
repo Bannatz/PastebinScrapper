b43601a7dfe098b82535e979323f771d143a5faf
Ban <kevintoptschij12012003@gmail.com>
2021-09-14 00:57:27 +0200

    Paar Lines Entfernt

--- main.py
+++ main.py
@@ -15,15 +15,17 @@ url = "https://pastebin.ga/"
 
 driver.get(url)
 #Zum Sichergehen das er die Seite richtig durchlädt
-time.sleep(2)
+#time.sleep(2)
 # Suchen von den Elementen die er braucht zum Leechen
 driver.find_element_by_id("gsc-i-id1").send_keys(str(input("Enter Keyword: ")))
 
 #driver.find_element_by_id("gsc-search-button gsc-search-button-v2").click()
 driver.find_element_by_xpath("//table[@class='gsc-search-box']/tbody[1]/tr[1]/td[2]/button[1]").send_keys(Keys.ENTER)
+
 time.sleep(5)
+
 driver.save_screenshot("Test1.png") # Debug Code
-#noch mal sicher gehen :D
-time.sleep(4)
+##noch mal sicher gehen :D
+#time.sleep(4)
 # Raus da!
 driver.quit()
