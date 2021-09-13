084c2d42484ae501e28a71fdaec98ed8c3352963
Ban <kevintoptschij12012003@gmail.com>
2021-09-14 01:13:45 +0200

    BeautifulSoup4 Import und Eingebaut (Hat aber Errors)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
 import time
 
 # Optionen :D
@@ -14,18 +15,25 @@ driver = webdriver.Firefox(options=options)
 url = "https://pastebin.ga/"
 
 driver.get(url)
-#Zum Sichergehen das er die Seite richtig durchlädt
-#time.sleep(2)
+
 # Suchen von den Elementen die er braucht zum Leechen
 driver.find_element_by_id("gsc-i-id1").send_keys(str(input("Enter Keyword: ")))
 
-#driver.find_element_by_id("gsc-search-button gsc-search-button-v2").click()
 driver.find_element_by_xpath("//table[@class='gsc-search-box']/tbody[1]/tr[1]/td[2]/button[1]").send_keys(Keys.ENTER)
 
 time.sleep(5)
+print(driver.current_url)
+current_url = driver.current_url
+
+url_id = "gsc-webResult gsc-result"
+
+urlsoup = BeautifulSoup(current_url, "html_parser")
+
+pastebin_data = urlsoup.find_all("div", attrs={"class": url_id})
+
+time.sleep(3)
 
 driver.save_screenshot("Test1.png") # Debug Code
-##noch mal sicher gehen :D
-#time.sleep(4)
+
 # Raus da!
 driver.quit()
