58054ff6e3f612da88ef630b36ed3dae0e206c08
MIRO <aaronbruser@gmail.com>
2021-09-17 17:01:43 +0000

    fixed for loop

--- test.py
+++ test.py
@@ -5,12 +5,9 @@ k = input("Enter Keyword: ")
 p = Scrapper.Scraping(k)
 
 for url in p:
-    try:
-        u = Scrapper.TextScrape(url)
-        if ":" in u:
-            print(u)
-        else:
-            print("Leider sind da nicht mehr drinne :(")
-    except TypeError:
-        print("Leider keine Combos :(")
-        sys.exit()
+    u = Scrapper.TextScrape(url)
+    if u is not None:
+        print(u)
+    else:
+        print("-----ENDE-----")
+sys.exit()
