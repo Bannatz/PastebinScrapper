e48bf0ecfadc58f9c7f8ecee8fa49a932877754f
MIRO <aaronbruser@gmail.com>
2021-09-17 22:28:12 +0000

    added fucking viel lol

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from modules.Scrapper import *
 from modules.format import *
+from modules.ProxyScraper import *
+from modules.checker import *
 import sys, os
+
+def get_proxys(type, timeout):
+    if type == "1":
+        p = scrape.http(str(timeout))
+    elif type == "2":
+        p = scrape.socks4(str(timeout))
+    elif type == "3":
+        p = scrape.socks5(str(timeout))
+    else:
+        print("something went wrong :o")
+        sys.exit()
+    
+    return p
 
 def get_mode(input):
 
@@ -14,18 +29,37 @@ def get_mode(input):
 
 
 print("""\
-Welcome
+Lets config ur session :)
 
-[1] all
+what do u want to scrape?
+
+[1] all (only good if u want user:pass)
 [2] only email:pass
 
 """)
 m = str(input())
+print("""\
+
+What kind of Proxys do want for checking?
+
+[1] http
+[2] socks4
+[3] socks5
+
+""")
+p_type = input()
+
+print("""\
+
+Now tell me the highest Proxy timeout u want^^ (min 50 | max 5000)
+
+""")
+timeout = input("Timeout: ")
 
 m = get_mode(m)
 if m == None:
     m = 0
-
+print("Now its scraping time :D\n")
 k = input("\nEnter Keywords: ")
 
 p = Scrapper.Scraping(k)
@@ -45,6 +79,9 @@ for c in list:
     c = format.combo(c)
     combo_list.append(c)
 
-print(combo_list) # temp
+proxy_list = get_proxys(p_type, timeout)
+print("Finshed Proxy checking!\nNow its Account checking Time :D\n")
+
+checker(combo_list, proxy_list)
 
 sys.exit()
