ec089cdf26081ca59010f913aabf0577cd384742
MIRO <aaronbruser@gmail.com>
2021-09-18 13:42:35 +0000

    added timeout

--- modules/proxychecker.py
+++ modules/proxychecker.py
@@ -7,7 +7,7 @@ def is_bad_proxy(pip):
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         urllib.request.install_opener(opener)
         req=urllib.request.Request('http://www.google.com')
-        sock=urllib.request.urlopen(req)
+        sock=urllib.request.urlopen(req, timeout=5000)
         return True
     except urllib.error.HTTPError as e:
         print('Error code: ', e.code)
