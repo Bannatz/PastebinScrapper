59a787716cd9ab5074a702aee909ea529956df79
Ban <kevintoptschij12012003@gmail.com>
2021-09-18 14:28:18 +0200

    blabliblubb

--- modules/proxychecker.py
+++ modules/proxychecker.py
@@ -1,30 +1,21 @@
 from ProxyScraper import *
-
-
 import urllib.request, socket, urllib.error,threading
 from multiprocessing import Process, Queue
-
-i = input("Bitte Timeout danke: ")
-print("socks4, socks5, http")
-t = str(input("Bitte Type eingeben: "))
-th = int(input("Wie viele Threads ?: "))
-if t == "socks4":
-    s = scrape.socks4(t)
-    
-if t == "socks5":
-    s = scrape.socks5(t)
-    
-if t == "http":
+def hallo():
+    i = input("Bitte Timeout danke: ")
+    print("http")
+    th = int(input("Wie viele Threads ?: "))
+  
     s = scrape.http(t)
-
+    return s
 
 def is_bad_proxy(pip):    
     try:
-        proxy_handler = urllib.request.ProxyHandler({'http': pip})
+        proxy_handler = urllib.request.ProxyHandler({"http": pip})
         opener = urllib.request.build_opener(proxy_handler)
         opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         urllib.request.install_opener(opener)
-        req=urllib.request.Request('http://www.example.com')  # change the URL to test here
+        req=urllib.request.Request('http://www.google.com')
         sock=urllib.request.urlopen(req)
     except urllib.error.HTTPError as e:
         print('Error code: ', e.code)
@@ -34,19 +25,21 @@ def is_bad_proxy(pip):
         return True
     return False
 
-def main():
+def main(s):
     for proxy in s:
-        if is_bad_proxy(proxy):
+        if is_bad_proxy(t, proxy):
             print("Bad Proxy: " + proxy)
         else:
             print("Good Proxy: " + proxy)
 
+def multikulti(s):
+    processes = [Process(target=main, args=(s)) for x in range(th)]
 
+    for p in processes:
+        p.start()
 
-processes = [Process(target=main, args=()) for x in range(th)]
-
-for p in processes:
-    p.start()
-
-for p in processes:
-    p.join()
+    for p in processes:
+        p.join()
+    
+s = hallo()
+multikulti(s)
